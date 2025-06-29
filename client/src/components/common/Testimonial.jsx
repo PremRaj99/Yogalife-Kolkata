5de2b8ee9f84f72bf3ed3a96a259ffd186a8f2a0
Prem Raj <premraj299341@gmail.com>
2025-06-29 19:52:26 +0530

    add more testimonial and pundicherry

--- client/src/components/common/Testimonial.jsx
+++ client/src/components/common/Testimonial.jsx
@@ -4,5 +4,7 @@ export default function Testimonial() {
     <video className="border border-black" controls src="./testimonial/Testimonial2.mp4"></video>
     <video className="border border-black" controls src="./testimonial/Testimonial3.mp4"></video>
     <video className="border border-black" controls src="./testimonial/Testimonial4.mp4"></video>
+    <video className="border border-black" controls src="./testimonial/Testimonial5.mp4"></video>
+    <video className="border border-black" controls src="./testimonial/Testimonial6.mp4"></video>
   </div> ;
 }
